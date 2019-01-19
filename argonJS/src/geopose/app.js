907a56af2341b3d9dd0fce268d4a6d5e0442cc75
ec <varikvi@yahoo.com>
2019-01-19 17:43:19 +0100

    first commit

--- argonJS/src/geopose/app.js
+++ argonJS/src/geopose/app.js
@@ -257,7 +257,7 @@ app.updateEvent.on(function (frame) {
         //infoText += "Waiting for geolocation..."+userPoseFIXED.position+"<br>";
         //navigator.geolocation.getCurrentPosition(function(p){locationElements[1].innerHTML = p.coords});
         infoText += "Waiting for geolocation...userPose<br> user: "+userPoseFIXED.poseStatus+"<br>";
-        infoText += "KN: "+String(Cesium)+"<br>";
+        //infoText += "KN: "+String(Cesium)+"<br>";
     }
     var geoBoxFixedPose = app.getEntityPose(geoBoxEntity, ReferenceFrame.FIXED);
     if (geoBoxFixedPose.poseStatus && Argon.PoseStatus.KNOWN) {

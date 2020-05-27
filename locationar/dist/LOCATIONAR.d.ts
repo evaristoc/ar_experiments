a84784877a8fc60430faa39f133acba79b76dd9d
ec <varikvi@yahoo.com>
2020-05-27 23:24:16 +0200

    locationar: deleting (security alerts)

--- locationar/dist/LOCATIONAR.d.ts
+++
@@ -1,7 +0,0 @@
-import { Source } from "./Source";
-import { LocationProvider } from "./LocationProvider";
-import { DirectionProvider } from "./DirectionProvider";
-import { BrightnessProvider } from "./BrightnessProvider";
-import { GpsLocation } from "./GpsLocation";
-import { GeoMath } from "./GeoMath";
-export { Source, LocationProvider, DirectionProvider, BrightnessProvider, GpsLocation, GeoMath };
--- locationar/dist/math/Vector3.d.ts
+++
@@ -1,7 +0,0 @@
-import { Vector3Type } from "../GeoMath";
-export declare class Vector3 implements Vector3Type {
-    x: number;
-    y: number;
-    z: number;
-    constructor(x?: number, y?: number, z?: number);
-}
